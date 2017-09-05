441aa0039dc262834713dbefeb010aa5baa2e7ed
Teerayut <teerayut.p1997@gmail.com>
2017-09-05 17:04:00 +0700

    Lab2 Complete

---
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class RPNCalculatorEngine : CalculatorEngine
+    {
+        public new string Process(string str)
+        {
+            List<string> parts = str.Split(' ').ToList<string>();
+            string result = "";
+            Stack<string> rpnStack = new Stack<string>();
+            foreach(string i in parts)
+            {
+                if (isNumber(i))
+                {
+                    rpnStack.Push(i);
+                }
+                else if (i == "")
+                {
+                    break;
+                }
+                else
+                {
+                    string secondOperand = rpnStack.Pop();
+                    string firstOperand = rpnStack.Pop();
+                    result = calculate(i,firstOperand,secondOperand);
+                    rpnStack.Push(result);
+                }
+            }
+            return result;
+        }
+    }
+}
